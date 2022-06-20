e7e03835acc6f70afaf72f1dd23bddc930ca0f71
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-06-20 22:52:07 +0300

    renderImg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { fetchImg } from './partials/fechImg';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
 refs.form.addEventListener('submit', submitHendler);
@@ -10,5 +13,43 @@ function submitHendler(event) {
   event.preventDefault();
   const { searchQuery: input } = refs.form;
   const searchQuery = input.value;
-  fetchImg(searchQuery.trim()).then(resp => console.log(resp));
+  fetchImg(searchQuery.trim()).then(data => renderImg(data));
 }
+
+function renderImg(obj) {
+  const dataArray = obj.data.hits;
+  console.log(dataArray);
+  const markup = dataArray
+    .map(el => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = el;
+      return `<div class="photo-card">
+  <img src="${webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  refs.gallery.innerHTML = markup;
+}
+
+// function renderImage() {}
--- src/partials/fechImg.js
+++ src/partials/fechImg.js
@@ -1,10 +1,14 @@
-function fetchImg(imgQuery) {
+import axios from 'axios';
+
+async function fetchImg(imgQuery) {
   const key = '28146499-54125423a2ce22d24a80a5e64';
   const imgType = 'photo';
   const orientation = 'horizontal';
   const safesearch = 'true';
   const url = `https://pixabay.com/api/?key=${key}&q=${imgQuery}&image_type=${imgType}&orientation=${orientation}&safesearch=${safesearch}`;
-  return fetch(url).then(response => response.json());
+  const query = await axios.get(url);
+  return query;
+  // fetch(url).then(response => response.json());
 }
 
 export { fetchImg };
