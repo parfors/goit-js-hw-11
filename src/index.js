e1e26d037d9f8858848e36da000a5d795dec326c
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-06-24 12:54:08 +0300

    error test

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ let lightbox = new SimpleLightbox('.gallery a', {
 function init() {
 const clearBtn = createClearBtn();
   refs.form.append(clearBtn);
+  console.log(clearBtn);
   clearBtnEl = document.querySelector('.clear');
   clearBtnEl.addEventListener('click', clearBtnHandler)
   refs.form.addEventListener('submit', submitHendler);
